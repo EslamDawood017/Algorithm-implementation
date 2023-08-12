3df79efb0cfdaa82749a3762dd2c40e1096171c3
Eslam Dawood <dawoodeslam017@gmail.com>
2023-08-12 23:30:55 +0300

    Add two new Algorithm

---
+++ Dynamic Programming – Longest Common Subsequence/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic_Programming___Longest_Common_Subsequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Text_01 = "HELLOWORLD";
+            string Text_02 = "OHELOD";
+
+            int n_Column = Text_01.Length;
+            int m_Row = Text_02.Length;
+
+            int[,] arr = new int[m_Row+1, n_Column+1];
+
+            for(int j = 0; j< n_Column+1; j++)
+            {
+                arr[0,j] = 0;
+            }
+
+            for (int i = 0; i < m_Row + 1; i++)
+            {
+                arr[i, 0] = 0;
+            }
+
+            for(int i = 1; i < m_Row + 1; i++)
+            {
+                for(int j = 1; j < n_Column+1; j++)
+                {
+                    if (Text_02[i-1] == Text_01[j-1])
+                    {
+                        arr[i, j] = arr[i-1,j-1] + 1 ;
+                    }
+                    else
+                    {
+                        arr[i, j] = Math.Max(arr[i,j-1], arr[i-1,j]);
+                    }
+                }
+            }
+
+            
+
+
+            Console.WriteLine(arr[m_Row , n_Column ]);
+        }
+    }
+}
---
+++ Dynamic Programming–Stagecoach Problem/Dynamic Programming–Stagecoach Problem.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+
+using namespace std;
+
+const int Max = 100;
+
+vector<string> Lebals = { "A" , "B" , "C" , "D" , "E" , "F" , "G" , "H" , "I" , "J" };
+
+vector<vector<int>> _data = {
+    {0, 2, 4, 3, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 7, 4, 6, 0, 0, 0},
+    {0, 0, 0, 0, 3, 2, 4, 0, 0, 0},
+    {0, 0, 0, 0, 4, 1, 5, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 1, 4, 0},
+    {0, 0, 0, 0, 0, 0, 0, 6, 3, 0},
+    {0, 0, 0, 0, 0, 0, 0, 3, 3, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 4},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+};
+
+struct State 
+{
+    string from;
+    string to;
+    int Cost;
+};
+
+
+int main()
+{
+    int n = _data.size(); 
+    vector<State>States(n);
+
+    States[n - 1].from = "";
+    States[n - 1].to = "";
+    States[n - 1].Cost = 0;
+
+    for (int i = n - 2; i >= 0; i--)
+    {
+        States[i] = { Lebals[i] , "" , INT_MAX };
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (_data[i][j] == 0) continue;
+
+            int newCost = _data[i][j] + States[j].Cost; 
+
+            if (newCost < States[i].Cost)
+            {
+                States[i].to = Lebals[j];
+                States[i].Cost = newCost;
+
+            }
+        }
+    }
+
+    cout <<  "Minimum Cost " << States[0].Cost;
+}
+
